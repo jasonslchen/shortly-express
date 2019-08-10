c53d395f634e5fccae7087291d63e2a7b950b149
helloitjase <jason.sl.chen@gmail.com>
2019-08-09 22:21:18 -0700

    pass assign a username test on sessions

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,35 +2,43 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-  // req.cookies req.cookies.username
-  // console.log(!!req.cookies);
-//  if(Object.keys(req.cookies).length === 0) {
-  // console.log('in', req.cookies.username);
-  models.Sessions.create()
+
+  return models.Sessions.create()
     .then((session) => {
       // console.log('is there any session?', session.insertId);
       req.session = session;
       models.Sessions.get({id: session.insertId})
         .then((sesh) => {
-          console.log("the sesh",sesh);
+          // console.log("the sesh",sesh);
           req.session.hash = sesh.hash;
           res.cookies.shortlyid = {value: sesh.hash};
+          // console.log("out", req);
           return req;
         })
         .then((request) => {
-          console.log('req', !!request.session.cookies);
-            if (!request.session.cookies) {
+          // console.log('big boss', request);
+          if (!!request.cookies) {
+            // console.log('typeof', (request.cookies.shortlyid))
+            // console.log('insertID', request.cookies.shortlyid);
+            models.Sessions.get({hash: request.cookies.shortlyid})
+            .then((value) => {
+              req.session.userId = value.userId;
+              req.session.user = {};
+              req.session.user.username = value.user.username;
+              // console.log('returned', value);
+              // console.log('userId', req.session);
+              // console.log('user', value.user.username);
+              // console.log('user', req.session)
+            })
+            .then(() => {
+              next();
+            })
 
-            } else {
-              console.log('hash', request.session.cookies.shortlyid.value);
-              models.Sessions.get({hash: request.session.cookies.shortlyid.value})
-              .then((session) => {
-                console.log('session1', session);
-              })
-
-            }
-
-
+          }
+          // models.Sessions.get({id: request.session.insertId})
+          // .then((value) => {
+          //   console.log('return', value);
+          // })
         })
         .then(() => {
           next();
@@ -40,7 +48,8 @@ module.exports.createSession = (req, res, next) => {
       console.log(':( ');
     });
   //  }
-
+ //have to refactor code
+ //put alot if if statements and elses to account for if userid and stuff is there
   // .then(())
 
   // .then(() => {
